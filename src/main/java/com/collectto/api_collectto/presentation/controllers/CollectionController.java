3a472b3eacf23d144c8a1cd422613639826db899
DeathZ <jose.joz46@hotmail.com>
2026-05-03 16:13:50 -0300

    style: fix indentation

--- src/main/java/com/collectto/api_collectto/presentation/controllers/CollectionController.java
+++ src/main/java/com/collectto/api_collectto/presentation/controllers/CollectionController.java
@@ -104,16 +104,16 @@ public class CollectionController {
         ));
         
         return new CollectionPageResponse(
-                output.collections().stream()
-                    .map(collection -> new CollectionPageResponse.CollectionSummaryResponse(
-                        collection.id(),
-                        collection.name(),
-                        collection.imagesURL()
-                    ))
-                    .toList(),
-                output.totalPages(),
-                output.totalElements(),
-                output.currentPage()
+            output.collections().stream()
+                .map(collection -> new CollectionPageResponse.CollectionSummaryResponse(
+                    collection.id(),
+                    collection.name(),
+                    collection.imagesURL()
+                ))
+                .toList(),
+            output.totalPages(),
+            output.totalElements(),
+            output.currentPage()
         );
     }
 
