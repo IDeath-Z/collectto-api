c05dfb31ac0e528c72034efacef9bef0bd58a143
DeathZ <jose.joz46@hotmail.com>
2026-05-17 22:18:30 -0300

    fix: correct spring bean mapping

--- src/main/java/com/collectto/api_collectto/infrastructure/config/ItemLikeConfig.java
+++ src/main/java/com/collectto/api_collectto/infrastructure/config/ItemLikeConfig.java
@@ -3,14 +3,22 @@ package com.collectto.api_collectto.infrastructure.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.collectto.api_collectto.application.usecases.itemlike.FetchItemLikesUseCase;
 import com.collectto.api_collectto.application.usecases.itemlike.LikeItemUseCase;
 import com.collectto.api_collectto.application.usecases.itemlike.UnlikeItemUseCase;
 import com.collectto.api_collectto.domain.ports.CollectionRepository;
 import com.collectto.api_collectto.domain.ports.ItemLikeRepository;
 import com.collectto.api_collectto.domain.ports.ItemRepository;
+import com.collectto.api_collectto.domain.ports.UserRepository;
 
 @Configuration
 public class ItemLikeConfig {
+
+    @Bean
+    FetchItemLikesUseCase fetchItemLikesUseCase(ItemLikeRepository itemLikeRepository, CollectionRepository collectionRepository, 
+        ItemRepository itemRepository, UserRepository userRepository) {
+        return new FetchItemLikesUseCase(itemLikeRepository, collectionRepository, itemRepository, userRepository);
+    }
 
     @Bean
     public LikeItemUseCase likeItemUseCase(ItemLikeRepository itemLikeRepository, ItemRepository itemRepository, CollectionRepository collectionRepository) {
