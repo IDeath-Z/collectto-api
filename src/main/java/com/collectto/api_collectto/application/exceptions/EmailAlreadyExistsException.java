04798950606299bfb59f1962e60ce45639e71ca5
DeathZ <jose.joz46@hotmail.com>
2026-05-29 18:31:46 -0300

    refactor: remove old exception classes

--- src/main/java/com/collectto/api_collectto/application/exceptions/EmailAlreadyExistsException.java
+++
@@ -1,9 +0,0 @@
-package com.collectto.api_collectto.application.exceptions;
-
-public class EmailAlreadyExistsException extends RuntimeException {
-    
-    public EmailAlreadyExistsException(String email) {
-        super("Email already exists: " + email);
-    }
-
-}
--- src/main/java/com/collectto/api_collectto/application/exceptions/UsernameAlreadyExistsException.java
+++
@@ -1,8 +0,0 @@
-package com.collectto.api_collectto.application.exceptions;
-
-public class UsernameAlreadyExistsException extends RuntimeException {
-    
-    public UsernameAlreadyExistsException(String username) {
-        super("Username already exists: " + username);
-    }
-}
